591c481edbeda8bc20172251744eb8d7cee839c9
fef8D <louis.gibson21@imperial.ac.uk>
2023-01-26 17:06:02 +0000

    Commit 4

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,3 +1,4 @@
+from numbers import Number
 class Polynomial:
     def __init__(self, coefs):
         self.coefficients = coefs  
@@ -14,8 +15,20 @@ class Polynomial:
             terms.append(str(coefs[0]))
         if self.degree() and coefs[1]:
             terms.append(f"{'' if coefs[1]==1 else coefs[1]}x")
-        
         terms += [f"{''if c == 1 else c}x^{d}"
                 for d,c in enumerate(coefs[2:],start=2) if c]
-        
         return " + ".join(reversed(terms)) or "0"
+    def __eq__(self, other):
+        return self.coefficients == other.coefficients
+    def __add__(self, other):
+        if isinstance(other, Polynomial):
+            common = min(self.degree(),other.degree()) +1
+            coefs = tuple(a+b for a,b in zip(self.coefficients,other.coefficients))
+            coefs += self.coefficients[common:] + other.coefficients[common:]
+        elif isinstance(other,Number):
+            return Polynomial((self.coefficients[0] + other,) + self.coefficients[1:])
+        else: 
+            return NotImplemented
+        return Polynomial(coefs)
+    def __radd__(self, other):
+        return self + other 
